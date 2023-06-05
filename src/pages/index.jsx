17f18fc09ae837e75977564f092634530a680fb2
Kanta <kant_coco87@icloud.com>
2023-06-05 21:10:15 +0900

    homeからAbout,Companyに飛べるようにした

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <>
+        <div>Home</div>
+        <a href="http://127.0.0.1:5173/about">aboutへ</a>
+        <a href="http://127.0.0.1:5173/company">companyへ</a>
+    </>
   )
 }
 
