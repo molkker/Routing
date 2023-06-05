2d76329ec0ad28e2712ae5c5ed12f0977ce55e3f
Kanta <kant_coco87@icloud.com>
2023-06-05 21:03:17 +0900

    first commit

---
+++ src/pages/company.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Company() {
+  return (
+    <>
+        <div>Company</div>
+        <a href="http://127.0.0.1:5173/about">aboutへ</a>
+    </>
+  )
+}
+
+export default Company
